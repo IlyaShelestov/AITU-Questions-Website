7bc8e3f76eb3c20f4421a371bcb1f2661c999eb7
IlyaShelestov <shelestov_ilya2905@mail.ru>
2025-05-20 18:37:27 +0500

    Change script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
 
-echo "Running database migrations…"
+#echo "Running database migrations…"
+#npm run migrate
 
-echo "Populating database..."
-npm run populate
+#echo "Populating database..."
+#npm run populate
 
 echo "Starting server…"
 npm start
